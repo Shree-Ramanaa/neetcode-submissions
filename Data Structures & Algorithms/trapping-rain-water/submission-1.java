6bdd064a9e52b782b957ca1740686bddefaa075a
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2026-05-26 22:47:22 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int trap(int[] h) {
+        Stack<Integer> st = new Stack<>();
+        int res = 0;
+
+        for(int i=0; i<h.length; i++){
+            while(!st.isEmpty() && h[i] >= h[st.peek()]){
+                int bottom = h[st.pop()];
+
+                if(!st.isEmpty()){
+                    int rightWall = h[i];
+                    int leftWall = h[st.peek()];
+                    int he = Math.min(rightWall, leftWall) - bottom;
+                    int w = i - st.peek() - 1;
+                    res += he * w;
+                }
+            }
+
+            st.push(i);
+        }
+
+        return res;
+    }
+}
