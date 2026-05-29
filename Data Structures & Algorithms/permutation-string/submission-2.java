24447ca36346ccec2baf7ce9b646f803b0655566
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2026-05-29 11:20:51 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] ltrs = new int[26];
+        Arrays.fill(ltrs, -1);
+
+        for (char ch : s1.toCharArray()) {
+            if (ltrs[ch - 'a'] == -1) {
+                ltrs[ch - 'a'] = 0;
+            }
+
+            ltrs[ch - 'a']++;
+        }
+
+        // for(int i:ltrs)System.out.print(i + " ");
+        // System.out.println();
+        int l = 0, r = 0;
+        for (r = 0; r < s2.length(); r++) {
+            // System.out.println(l + " " + r);
+            if (r - l == s1.length())
+                return true;
+            if (ltrs[s2.charAt(r) - 'a'] == -1) {
+                while (l < r) {
+                    ltrs[s2.charAt(l++) - 'a']++;
+                }
+                l = r + 1;
+                // for(int i:ltrs)System.out.print(i + " ");
+                // System.out.println();
+                continue;
+            }
+
+            if (ltrs[s2.charAt(r) - 'a'] == 0) {
+                while (ltrs[s2.charAt(r) - 'a'] == 0) {
+                    ltrs[s2.charAt(l++) - 'a']++;
+                }
+            }
+
+            ltrs[s2.charAt(r) - 'a']--;
+
+            // for(int i:ltrs)System.out.print(i + " ");
+            // System.out.println();
+        }
+
+        return r-l == s1.length();
+    }
+}
