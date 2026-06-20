a91b2c21fcab0b12a778d1234be9e5a7623f3d3b
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2026-06-20 12:51:55 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    HashSet<String> hs = new HashSet<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+     List<List<Integer>> res= new ArrayList<>();
+        rec(0,nums,new ArrayList<>(), res);
+        return res;
+    }
+
+    private void rec(int i, int[] nums, List<Integer> subset, List<List<Integer>> res){
+        if (i >= nums.length) {
+            List<Integer> sb = new ArrayList<>(subset);
+            Collections.sort(sb);
+            if(hs.add(sb.toString()) == false)return;
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        rec(i+1, nums, subset, res);
+        subset.remove(subset.size() - 1);
+        rec(i+1, nums, subset, res);
+    }
+}
