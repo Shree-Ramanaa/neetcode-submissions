2085ca10872b58c5d8bea4a5212987ad402ae526
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2026-06-21 17:45:41 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    private List<String> res = new ArrayList<>();
+    private String[] digitToChar = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "qprs", "tuv", "wxyz"
+    };
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.equals(""))return res;
+        gen(digits, 0, new StringBuilder());
+        return res;
+    }
+
+    private void gen(String digits, int i, StringBuilder sb){
+        if(digits.length() == i){
+            res.add(sb.toString());
+            return;
+        }
+
+        for(int j=0; j< digitToChar[digits.charAt(i) - '0'].length(); j++){
+                char ch = digitToChar[digits.charAt(i) - '0'].charAt(j);
+                sb.append(ch);
+                gen(digits, i+1, sb);
+                sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
