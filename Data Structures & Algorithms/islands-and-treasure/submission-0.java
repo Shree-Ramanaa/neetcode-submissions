be912a72952184af0d7eae73ddffbfda6d0d4590
Shree-Ramanaa <100988994+Shree-Ramanaa@users.noreply.github.com>
2026-06-28 00:37:32 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        HashSet<String> visited = new HashSet<>();
+        Queue<int[]> q = new ArrayDeque<>();
+        int dirs[][] = new int[][] {{0,1}, {0,-1}, {-1,0}, {1, 0}};
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 0){
+                    q.add(new int[]{i,j});
+                }
+            }
+        }
+        int rounds = 1;
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            while(size-- > 0){
+                int cur[] = q.poll();
+
+                for(int[] dir: dirs){
+                    int r = cur[0] + dir[0];
+                    int c = cur[1] + dir[1];
+                    if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == -1 || visited.contains(r+" "+c) || grid[r][c] == 0){
+                        continue;
+                    }
+
+                    grid[r][c] = rounds;
+                    q.add(new int[]{r,c});
+                    visited.add(r+" "+c);
+                }
+            }
+
+            rounds++;
+        }
+    }
+}
